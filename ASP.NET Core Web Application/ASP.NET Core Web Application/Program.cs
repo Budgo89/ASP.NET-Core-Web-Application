5f8a14188263a648d3e837ee783dbce3c84c928d
LAPTOP-PSEPRFOJ\Максим <budgo@mail.ru>
2022-02-02 19:55:20 +0300

    основа

---
+++ ASP.NET Core Web Application/ASP.NET Core Web Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASP.NET_Core_Web_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
