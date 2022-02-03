463635b15795e1374e1f7d81ab31731a72eead93
LAPTOP-PSEPRFOJ\Максим <budgo@mail.ru>
2022-02-04 00:26:36 +0300

    Lesson1

---
+++ ASP.NET Core Web Application/ASP.NET Core Web Application/PostModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ASP.NET_Core_Web_Application
+{
+    public class PostModel
+    {
+        public override string ToString() => $"{UserId}\n{Id}\n{Title}\n{Body}\n";
+
+        public uint UserId { get; set; }
+        public uint Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
--- ASP.NET Core Web Application/ASP.NET Core Web Application/Program.cs
+++ ASP.NET Core Web Application/ASP.NET Core Web Application/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ASP.NET_Core_Web_Application
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tasks = new List<Task<PostModel>>();
+            for (uint i = 4; i <= 13; i++)
+                tasks.Add(GetPostByID(i));
+
+            Task.WaitAll(tasks.ToArray());
+
+            using (var sw = new StreamWriter(File.Create("result.txt")))
+            {
+                foreach (var task in tasks)
+                    if (task.IsCompleted && task.Exception == null)
+                        sw.WriteLine(task.Result);
+            }
+		}
+        private static async Task<PostModel> GetPostByID(uint postID)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{postID}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                await using var responseStream = await response.Content.ReadAsStreamAsync();
+                if (await JsonSerializer.DeserializeAsync<PostModel>(responseStream, new JsonSerializerOptions(JsonSerializerDefaults.Web)) is PostModel post)
+                    return post;
+                else
+                    throw new Exception($"Error content");
+            }
+            else
+            {
+                throw new Exception($"Eroor code: {response.StatusCode}");
+            }
         }
     }
 }
